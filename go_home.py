30303e4699e8611b019e6582d436370f55039a4f
f1etcher_pyt <f1etcher0208.1@gmail.com>
2026-01-31 22:17:47 +0200

    Boolean basic practice

---
+++ go_home.py
@@ -0,0 +1,5 @@
+work_done = True
+day_finished = False
+
+can_go_home = work_done or day_finished
+print(can_go_home)
---
+++ make_cake.py
@@ -0,0 +1,6 @@
+has_eggs = True
+has_flour = True
+has_sugar = True
+
+can_make_cake = has_eggs and has_flour and has_sugar
+print(can_make_cake)
